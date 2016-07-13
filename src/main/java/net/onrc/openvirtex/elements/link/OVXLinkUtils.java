78d782a60e1f384df72e2de4dcdff6491a5bc9b3
byyu <koreagood13@gmail.com>
2016-07-13 17:02:48 +0900

    test21
    test21

--- src/main/java/net/onrc/openvirtex/elements/link/OVXLinkUtils.java
+++ src/main/java/net/onrc/openvirtex/elements/link/OVXLinkUtils.java
@@ -304,7 +304,7 @@ public class OVXLinkUtils {
     
     //byyu
     public void rewriteEdgeMatch(final OFMatch match) {
-    	match.setWildcards(match.getWildcards() & (~OFMatch.OFPFW_NW_DST_ALL) & (~OFMatch.OFPFW_NW_SRC_ALL));// & (~OFMatch.OFPFW_DL_TYPE));
+    	match.setWildcards(match.getWildcards() & (~OFMatch.OFPFW_NW_DST_ALL) & (~OFMatch.OFPFW_NW_SRC_ALL) & (~OFMatch.OFPFW_DL_TYPE));
     }
 
     /**
