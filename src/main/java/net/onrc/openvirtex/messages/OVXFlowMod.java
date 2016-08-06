e162169bda511cc706eae77bcad10d7099f5cd71
byyu <koreagood13@gmail.com>
2016-08-07 01:34:46 +0900

    test124

--- src/main/java/net/onrc/openvirtex/messages/OVXFlowMod.java
+++ src/main/java/net/onrc/openvirtex/messages/OVXFlowMod.java
@@ -158,7 +158,9 @@ public class OVXFlowMod extends OFFlowMod implements Devirtualizable {
         boolean edgeOut=true;
         boolean duflag=false;
         this.match.setWildcards(coreForceSetWcd());
-        this.hardTimeout = 1000;
+        if(this.idleTimeout==0){
+        	this.idleTimeout = 5;
+        }
         try {
             if (inPort.isEdge()) {
             	match.setWildcards(match.getWildcards() & (~OFMatch.OFPFW_DL_TYPE));
